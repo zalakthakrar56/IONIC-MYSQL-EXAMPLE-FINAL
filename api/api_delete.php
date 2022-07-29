47f7834951743e7f9c1aa5130d022e2b4f10d18c
machenic546 <86463229+machenic546@users.noreply.github.com>
2022-07-29 19:59:26 +0530

    remove one line in update

--- api/api_delete.php
+++ api/api_delete.php
@@ -5,7 +5,6 @@ header("Access-Control-Allow-Methods: POST,GET,PUT,DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,Access-Control-Allow-Methods");
 
 $id = $_GET['id'];
-
 $con = mysqli_connect("localhost","root","","ionic") or die(mysqli_error($con));
 $query = "DELETE FROM `register` where id=$id";
 mysqli_query($con,$query) or die(mysqli_error($con));
