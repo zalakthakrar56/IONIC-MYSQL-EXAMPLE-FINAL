c27c0a61c009ab3830ce09f768c0ad154b023213
machenic546 <86463229+machenic546@users.noreply.github.com>
2022-07-29 20:13:01 +0530

    change in delete

--- api/api_delete.php
+++ api/api_delete.php
@@ -3,7 +3,6 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Access-Control-Allow-Methods: POST,GET,PUT,DELETE");
 header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With,Access-Control-Allow-Methods");
-
 $id = $_GET['id'];
 $con = mysqli_connect("localhost","root","","ionic") or die(mysqli_error($con));
 $query = "DELETE FROM `register` where id=$id";
