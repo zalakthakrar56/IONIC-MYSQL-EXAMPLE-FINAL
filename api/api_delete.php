122e402cb07755e3d929aaa00fdfcd4293a8a88b
machenic546 <86463229+machenic546@users.noreply.github.com>
2022-07-29 20:18:52 +0530

    add last line

--- api/api_delete.php
+++ api/api_delete.php
@@ -8,3 +8,4 @@ $con = mysqli_connect("localhost","root","","ionic") or die(mysqli_error($con));
 $query = "DELETE FROM `register` where id=$id";
 mysqli_query($con,$query) or die(mysqli_error($con));
 echo json_encode("Record Delete");
+?>
